da6b9f6a3b16893501c97afef84a8c22f34c1751
Andreas Antoniades <141181847+Antrikas@users.noreply.github.com>
2024-08-05 15:06:06 +0300

    Update Landing.jsx

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -7,27 +7,16 @@ const Landing = () => {
       <header>
         <div className="header__container">
           <div className="header__description">
-            <h1>Cyprus's Auto Service Platform</h1>
+            <h1>America's most awarded online library platform</h1>
             <h2>
-              Find your Auto Service <span className="orange">Now</span>
+              Find your dream book with <span className="purple">Library</span>
             </h2>
-            <div className="nav__btns">
-            <a href="#Mechanics">
-              <button className="btn">Auto Mechanics</button>
-              </a>
-              <a href="#Auto Body Technician">
-              <button className="btn">Auto Body Technician</button>
-              </a>
-              <a href="#Automotive Painter">
-              <button className="btn">Automotive Painter</button>
-              </a>
-              <a href="#Auto Glass Technician">
-              <button className="btn">Auto Glass Technician</button>
+            <a href="#features">
+              <button className="btn">Browse books</button>
             </a>
-            </div>
           </div>
           <figure className="header__img--wrapper">
-           <img src="../assets/wallpaper.jpeg" alt=""></img>
+            <img src={UndrawBooks} alt="" />
           </figure>
         </div>
       </header>
