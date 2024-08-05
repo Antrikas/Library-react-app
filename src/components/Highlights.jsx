e2d7c6b7d63eb140bbcd2dbff1f63ef900bc1ed9
Andreas Antoniades <141181847+Antrikas@users.noreply.github.com>
2024-08-05 15:10:40 +0300

    Update Highlights.jsx

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -8,18 +8,18 @@ const Highlights = () => {
       <div className="container">
         <div className="row">
           <h2 className="section__title">
-            Why choose <span className="purple">Us</span>
+            Why choose <span className="purple">Library</span>
           </h2>
           <div className="highlight__wrapper">
             <Highlight
               icon={<FontAwesomeIcon icon="bolt" />}
               title="Easy and Quick"
-              para="Get access to an Auto service specialist online instantly."
+              para="Get access to the book you purchased online instantly."
             />
             <Highlight
               icon={<FontAwesomeIcon icon="book-open" />}
-              title="100+ Servises"
-              para="CY Auto Platform has services in all auto categories."
+              title="10,000+ Books"
+              para="Library has books in all your favourite categories."
             />
             <Highlight
               icon={<FontAwesomeIcon icon="tags" />}
